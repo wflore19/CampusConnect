80b530de7b5a2a300e416ec0cbeee0655e4ed794
wflore19 <wflore19@montgomerycollege.edu>
2024-11-10 19:16:22 -0500

    add: animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,36 @@ export default {
         success: colors.green[600],
         warning: colors.yellow[400],
       },
+      keyframes: {
+        'loader-shimmer': {
+          '0%, 100%': { opacity: '0.1' },
+          '50%': { opacity: '0.8' },
+        },
+        'modal-animation': {
+          '0%': { opacity: '0', transform: 'scale(0)' },
+          '100%': { opacity: '1', transform: 'scale(1)' },
+        },
+        'modal-animation-mobile': {
+          '0%': { bottom: '-100vh' },
+          '100%': { bottom: '0vh' },
+        },
+        'modal-shader-animation': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '0.75' },
+        },
+        'toast-animation': {
+          '0%': { transform: 'translateX(-150%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+        'toast-shader-animation': {
+          '0%': { width: '100%' },
+          '100%': { width: '0%' },
+        },
+        'slide-from-left': {
+          '0%': { left: '-100%' },
+          '100%': { left: '0%' },
+        },
+      },
     },
   },
   plugins: [],
