6e1738a4bdf810b42a2f1a7239ef984b2510396c
wflore19 <wflore19@montgomerycollege.edu>
2024-11-13 03:17:58 -0500

    fix: design

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -1,13 +1,10 @@
 import {
-  Link,
-  type LinkProps,
-  NavLink,
+    Link,
+    type LinkProps,
+    NavLink,
+    Form as RemixForm,
 } from '@remix-run/react';
-import React, {
-  type PropsWithChildren,
-  useContext,
-  useState,
-} from 'react';
+import React, { type PropsWithChildren, useContext, useState } from 'react';
 import { LogOut, Menu, X } from 'react-feather';
 
 import { IconButton } from './icon-button';
@@ -15,189 +12,193 @@ import { Text } from './text';
 import { cx } from '../utils/cx';
 
 type DashboardContextValue = {
-  open: boolean;
-  setOpen(open: boolean): void;
+    open: boolean;
+    setOpen(open: boolean): void;
 };
 
 const DashboardContext = React.createContext<DashboardContextValue>({
-  open: false,
-  setOpen: () => {},
+    open: false,
+    setOpen: () => {},
 });
 
-
 export const Dashboard = ({ children }: PropsWithChildren) => {
-  const [open, setOpen] = useState<boolean>(false);
+    const [open, setOpen] = useState<boolean>(false);
 
-  return (
-    <DashboardContext.Provider value={{ open, setOpen }}>
-      <main>{children}</main>
-    </DashboardContext.Provider>
-  );
+    return (
+        <DashboardContext.Provider value={{ open, setOpen }}>
+            <main>{children}</main>
+        </DashboardContext.Provider>
+    );
 };
 
 Dashboard.CloseMenuButton = function CloseMenuButton() {
-  const { setOpen } = useContext(DashboardContext);
+    const { setOpen } = useContext(DashboardContext);
 
-  function onClick() {
-    setOpen(false);
-  }
+    function onClick() {
+        setOpen(false);
+    }
 
-  return (
-    <div className="md:hidden">
-      <IconButton
-        className='text-white'
-        icon={<X />}
-        onClick={onClick}
-        shape="circle"
-      />
-    </div>
-  );
+    return (
+        <div className="md:hidden">
+            <IconButton
+                className="text-white"
+                icon={<X />}
+                onClick={onClick}
+                shape="circle"
+            />
+        </div>
+    );
 };
 
 Dashboard.CompanyLogo = function CompanyLogo() {
-  return (
-    <Link to="/" className="flex items-center gap-1 p-6">
-      <img 
-        alt="Company Logo" 
-        src="/images/logo.png" 
-        className="h-8 w-auto"
-      />
-    </Link>
-  );
+    return (
+        <Link to="/" className="flex items-center gap-1 p-6">
+            <img
+                alt="Company Logo"
+                src="/images/logo.png"
+                className="h-8 w-auto"
+            />
+        </Link>
+    );
 };
 
 const itemClassName = cx(
-  'box-border flex w-full items-center gap-3 p-4',
-  'transition-all duration-200',
-  'hover:bg-blue-100 hover:text-black',
-  'aria-[current="page"]:bg-blue-600 aria-[current="page"]:border-l-4 aria-[current="page"]:hover:text-white',
+    'box-border flex w-full items-center gap-3 p-4',
+    'transition-all duration-200',
+    'hover:bg-blue-100 hover:text-black',
+    'aria-[current="page"]:border-l-4 aria-[current="page"]:bg-blue-600 aria-[current="page"]:hover:text-white'
 );
 
 Dashboard.LogoutForm = function LogoutForm() {
-  return (
-      <Link to="/" className={cx(itemClassName, 'hover:text-red', 'mt-auto w-full')} type="submit">
-        <LogOut />
-        Log Out
-      </Link>
-  );
+    return (
+        <RemixForm action="/logout" className="mt-auto w-full" method="post">
+            <button
+                className={cx(itemClassName, 'hover:text-black')}
+                type="submit"
+            >
+                <LogOut />
+                Log Out
+            </button>
+        </RemixForm>
+    );
 };
 
 Dashboard.MenuButton = function MenuButton() {
-  const { setOpen } = useContext(DashboardContext);
+    const { setOpen } = useContext(DashboardContext);
 
-  function onClick() {
-    setOpen(true);
-  }
+    function onClick() {
+        setOpen(true);
+    }
 
-  return (
-    <IconButton
-      className="flex h-fit w-fit md:hidden text-white"
-      icon={<Menu />}
-      onClick={onClick}
-      shape="circle"
-    />
-  );
+    return (
+        <IconButton
+            className="flex h-fit w-fit text-white md:hidden"
+            icon={<Menu />}
+            onClick={onClick}
+            shape="circle"
+        />
+    );
 };
 
 Dashboard.Navigation = function Navigation({ children }: PropsWithChildren) {
-  return <nav className="w-full">{children}</nav>;
+    return <nav className="w-full">{children}</nav>;
 };
 
 type DashboardNavigationLinkProps = {
-  icon: JSX.Element;
-  label: string;
-  pathname: string;
-  prefetch?: LinkProps['prefetch'];
+    icon: JSX.Element;
+    label: string;
+    pathname: string;
+    prefetch?: LinkProps['prefetch'];
 };
 
 Dashboard.NavigationLink = function NavigationLink({
-  icon,
-  label,
-  pathname,
-  prefetch,
+    icon,
+    label,
+    pathname,
+    prefetch,
 }: DashboardNavigationLinkProps) {
-  const { setOpen } = useContext(DashboardContext);
+    const { setOpen } = useContext(DashboardContext);
 
-  function onClick() {
-    setOpen(false);
-  }
+    function onClick() {
+        setOpen(false);
+    }
 
-  return (
-    <li>
-      <NavLink
-        className={itemClassName}
-        onClick={onClick}
-        prefetch={prefetch}
-        to={pathname}
-      >
-        {icon} {label}
-      </NavLink>
-    </li>
-  );
+    return (
+        <li>
+            <NavLink
+                className={itemClassName}
+                onClick={onClick}
+                prefetch={prefetch}
+                to={pathname}
+            >
+                {icon} {label}
+            </NavLink>
+        </li>
+    );
 };
 
 Dashboard.NavigationList = function NavigationList({
-  children,
+    children,
 }: PropsWithChildren) {
-  return <ul className="flex flex-col">{children}</ul>;
+    return <ul className="flex flex-col">{children}</ul>;
 };
 
 Dashboard.Page = function Page({
-  children,
-  className,
+    children,
+    className,
 }: PropsWithChildren<{ className?: string }>) {
-  return (
-    <section
-      className={cx(
-        '@container box-border flex min-h-screen flex-col gap-4',
-        'p-4 pb-24',
-        'md:ml-[270px] md:p-6 md:pb-16',
-        className
-      )}
-    >
-      {children}
-    </section>
-  );
+    return (
+        <section
+            className={cx(
+                '@container box-border flex min-h-screen flex-col gap-4',
+                'p-4 pb-24',
+                'md:ml-[270px] md:p-6 md:pb-16',
+                className
+            )}
+        >
+            {children}
+        </section>
+    );
 };
 
-
 Dashboard.Sidebar = function Sidebar({ children }: PropsWithChildren) {
-  const { open } = useContext(DashboardContext);
+    const { open } = useContext(DashboardContext);
 
-  return (
-    <aside
-      className={cx(
-        'fixed left-0 h-screen w-[270px] flex-col items-start gap-4 overflow-auto border-r border-r-gray-200 bg-blue-900 text-white',
-        'md:flex',
-        open
-          ? 'z-10 flex w-[calc(100%-4rem)] animate-[slide-from-left_300ms] md:hidden'
-          : 'hidden'
-      )}
-    >
-      {children}
-    </aside>
-  );
+    return (
+        <aside
+            className={cx(
+                'fixed left-0 h-[calc(100%-65px)] w-[270px] flex-col items-start gap-4 overflow-auto border-r border-r-gray-200 bg-blue-900 text-white',
+                'md:flex',
+                open
+                    ? 'z-10 flex w-[calc(100%-4rem)] animate-[slide-from-left_300ms] md:hidden'
+                    : 'hidden'
+            )}
+        >
+            {children}
+        </aside>
+    );
 };
 
 Dashboard.Subheader = function Subheader({ children }: PropsWithChildren) {
-  return <div className="flex justify-between gap-4">{children}</div>;
+    return <div className="flex justify-between gap-4">{children}</div>;
 };
 
 Dashboard.Title = function Title({ children }: PropsWithChildren) {
-  return <Text variant="2xl">{children}</Text>;
+    return <Text variant="2xl">{children}</Text>;
 };
 
 Dashboard.Header = function Header() {
-  const { open } = useContext(DashboardContext);
+    const { open } = useContext(DashboardContext);
 
-  return (
-    <header className="sticky top-0 z-10 bg-blue-900 border-b border-gray-100 border-opacity-25 p-4 md:hidden">
-      <div className="flex items-center justify-between">
-        {open ? 
-        <Dashboard.CloseMenuButton /> :
-        <Dashboard.MenuButton /> 
-        }
-      </div>
-    </header>
-  );
+    return (
+        <header className="sticky top-0 z-10 border-b border-gray-100 border-opacity-25 bg-blue-900 p-4 md:hidden">
+            <div className="flex items-center justify-between">
+                {open ? (
+                    <Dashboard.CloseMenuButton />
+                ) : (
+                    <Dashboard.MenuButton />
+                )}
+            </div>
+        </header>
+    );
 };
--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -15,13 +15,41 @@ export default function SignUp() {
     const { googleAuthUrl } = useLoaderData<typeof loader>();
 
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-            <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
-                <h1 className="mb-6 text-center text-2xl font-bold">Sign Up</h1>
-                <div>
-                    {!!googleAuthUrl && <GoogleButton href={googleAuthUrl} />}
+        <div className="flex min-h-screen flex-col">
+            <header className="bg-blue-600 py-4 sm:py-6">
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-col items-center justify-between sm:flex-row">
+                        <div className="mb-4 text-center sm:mb-0 sm:text-left">
+                            <h1 className="text-3xl font-bold text-white sm:text-4xl">
+                                CampusConnect
+                            </h1>
+                            <p className="mt-1 text-lg text-white sm:mt-2 sm:text-xl">
+                                Building Community on Campus
+                            </p>
+                        </div>
+                        <Link
+                            to="/home"
+                            className="rounded bg-white px-4 py-2 text-sm font-bold text-blue-600 transition duration-300 hover:bg-blue-50 sm:text-base"
+                        >
+                            Go to Dashboard
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            </header>
+            <main className="flex flex-grow items-center justify-center bg-gray-100">
+                <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+                    <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
+                        <h1 className="mb-6 text-center text-2xl font-bold">
+                            Sign Up
+                        </h1>
+                        <div>
+                            {!!googleAuthUrl && (
+                                <GoogleButton href={googleAuthUrl} />
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
