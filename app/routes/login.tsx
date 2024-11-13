fc2c26758048011f21914610f9aeae8907dac7cd
wflore19 <wflore19@montgomerycollege.edu>
2024-11-13 01:53:42 -0500

    fix: prettier formatting

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,27 +1,27 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { getGoogleAuthURL } from "../utils/auth";
-import { GoogleButton } from "~/components/google-button";
+import type { LoaderFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { getGoogleAuthURL } from '../utils/auth';
+import { GoogleButton } from '~/components/google-button';
 
 export const loader: LoaderFunction = async () => {
-  const googleAuthUrl = getGoogleAuthURL()
+    const googleAuthUrl = getGoogleAuthURL();
 
-  return {
-    googleAuthUrl,
-  };
+    return {
+        googleAuthUrl,
+    };
 };
 
 export default function LoginPage() {
-  const { googleAuthUrl } = useLoaderData<typeof loader>();
+    const { googleAuthUrl } = useLoaderData<typeof loader>();
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
-        <h1 className="text-2xl font-bold mb-6 text-center">Log In</h1>
-        <div>
-          {!!googleAuthUrl && <GoogleButton href={googleAuthUrl} />}
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+            <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
+                <h1 className="mb-6 text-center text-2xl font-bold">Log In</h1>
+                <div>
+                    {!!googleAuthUrl && <GoogleButton href={googleAuthUrl} />}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
