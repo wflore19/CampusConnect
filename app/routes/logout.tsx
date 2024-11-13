c0ce17acf6ef5610b69bbd8bd74efb4db450b1e0
wflore19 <wflore19@montgomerycollege.edu>
2024-11-13 03:18:57 -0500

    add: logout flow

---
+++ app/routes/logout.tsx
@@ -0,0 +1,27 @@
+import {
+    type ActionFunctionArgs,
+    type LoaderFunctionArgs,
+    redirect,
+} from '@remix-run/node';
+
+import {
+    destroySession,
+    ensureUserAuthenticated,
+    getSession,
+} from '~/utils/session.server';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+    await ensureUserAuthenticated(request);
+
+    return {};
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+    const session = await getSession(request);
+
+    return redirect('/login', {
+        headers: {
+            'Set-Cookie': await destroySession(session),
+        },
+    });
+}
