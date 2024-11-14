eb1640fe0315adef4220f5eaad0640cd1d7358e1
wflore19 <wflore19@montgomerycollege.edu>
2024-11-13 20:57:56 -0500

    fix: design on mobile

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -9,41 +9,32 @@ export function Header({ name = '', imageUrl = '' }: HeaderProps) {
     return (
         <header className="bg-blue-600 py-4 sm:py-6">
             <div className="container mx-auto px-4">
-                <div className="flex flex-col items-center justify-between sm:flex-row md:flex-row">
-                    <div className="mb-4 flex text-center sm:mb-0 sm:text-left">
+                <div className="flex flex-row items-center justify-between">
+                    <div className="flex">
                         <Link to="/" className="pr-2 pt-1">
                             <img
                                 alt="Company Logo"
                                 src="/images/logo.png"
-                                className="h-12 w-auto"
+                                className="h-6 w-auto md:h-12"
                             />
                         </Link>
-                        <div className="flex-col">
-                            <h1 className="text-3xl font-bold text-white sm:text-4xl">
-                                CampusConnect
-                            </h1>
-                            <p className="mt-1 text-lg text-white sm:mt-2 sm:text-xl">
-                                Building Community on Campus
-                            </p>
-                        </div>
+                        <h1 className="text-2xl text-white sm:text-4xl md:text-3xl">
+                            CampusConnect
+                        </h1>
                     </div>
                     {name ? (
                         <div className="flex items-center space-x-4">
-                            <img
-                                src={
-                                    imageUrl
-                                        ? imageUrl
-                                        : 'https://ui-avatars.com/api/?name=' +
-                                          name
-                                }
-                                alt=""
-                                className="h-8 w-8 rounded-full object-cover"
-                            />
-                            <Link
-                                to="/home"
-                                className="rounded bg-white px-4 py-2 text-sm font-bold text-blue-600 transition duration-300 hover:bg-blue-50 sm:text-base"
-                            >
-                                Go to Dashboard
+                            <Link to="/home">
+                                <img
+                                    src={
+                                        imageUrl
+                                            ? imageUrl
+                                            : 'https://ui-avatars.com/api/?name=' +
+                                              name
+                                    }
+                                    alt=""
+                                    className="h-8 w-8 rounded-full object-cover"
+                                />
                             </Link>
                         </div>
                     ) : (
