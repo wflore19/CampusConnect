04e2d122fa66f8d91d9ecd7f8fd2848c4c6c0454
wflore19 <wflore19@montgomerycollege.edu>
2024-11-15 01:35:57 -0500

    chore: clean up auth

--- app/routes/oauth.google.tsx
+++ app/routes/oauth.google.tsx
@@ -1,10 +1,12 @@
 import type { LoaderFunction } from '@remix-run/node';
-import { redirect, Session } from '@remix-run/node';
-import { db } from 'db/src';
-import { getGoogleUser } from '~/utils/auth';
-import { APP_URL } from '~/utils/env';
-import { uploadImageFromCDN } from '~/utils/s3-config.server';
-import { commitSession, getSession } from '~/utils/session.server';
+import { redirect } from '@remix-run/node';
+import {
+    findUserByEmail,
+    getGoogleUser,
+    loginExistingUser,
+    signupNewUser,
+} from '~/utils/auth';
+import { getSession } from '~/utils/session.server';
 
 export const loader: LoaderFunction = async ({ request }) => {
     const url = new URL(request.url);
@@ -17,10 +19,8 @@ export const loader: LoaderFunction = async ({ request }) => {
     }
 
     try {
-        // Fetch Google user data
-        const googleUser: GoogleUser = await getGoogleUser(code);
+        const googleUser = await getGoogleUser(code);
 
-        // Check if user exists
         const existingUser = await findUserByEmail(googleUser.email);
 
         if (!existingUser) return signupNewUser(googleUser, session);
@@ -30,129 +30,3 @@ export const loader: LoaderFunction = async ({ request }) => {
         throw new Error('Failed to authenticate Google user');
     }
 };
-
-// Define types for better type safety
-interface GoogleUser {
-    email: string;
-    name: string;
-    picture: string;
-}
-
-interface User {
-    name: string;
-    createdAt: Date | null;
-    id: string;
-    imageUrl: string | null; // Change this to allow null
-    location: string | null;
-    updatedAt: Date | null;
-    email: string;
-    interests: string[] | null;
-    major: string;
-    year: string;
-}
-
-/**
- * Find a user by their email
- * @param email - The email of the user
- * @returns The user object if found, otherwise undefined
- */
-async function findUserByEmail(email: string): Promise<User | undefined> {
-    const userProfile = await db
-        .selectFrom('users')
-        .selectAll()
-        .where('email', '=', email)
-        .executeTakeFirst();
-
-    return userProfile;
-}
-
-/**
- * Logs in an existing user and redirects them to the home page
- * @param user - The user object
- * @param session - The session object
- * @returns A redirect response
- */
-async function loginExistingUser(user: User, session: Session) {
-    session.set('user_id', user.id);
-
-    const redirectUrl = new URL(`${APP_URL}/home`);
-    return redirect(redirectUrl.toString(), {
-        headers: {
-            'Set-Cookie': await commitSession(session),
-        },
-    });
-}
-
-/**
- * Signs up a new user, adds them to the database, and redirects them to the home page
- * @param googleUser - The Google user object
- * @param session - The session object
- * @returns A redirect response
- */
-async function signupNewUser(googleUser: GoogleUser, session: Session) {
-    const imageUrl = await uploadAndProcessImage(googleUser);
-    const newUser = await createNewUser(googleUser, imageUrl);
-
-    session.set('user_id', newUser.id);
-
-    const redirectUrl = new URL(`${APP_URL}/home`);
-    return redirect(redirectUrl.toString(), {
-        headers: {
-            'Set-Cookie': await commitSession(session),
-        },
-    });
-}
-
-/**
- * Uploads the image from the Google user to DigitalOcean Spaces
- * @param googleUser - The Google user object
- * @returns The URL of the uploaded image
- */
-async function uploadAndProcessImage(googleUser: GoogleUser): Promise<string> {
-    const spacesImageUrl = await uploadImageFromCDN(
-        googleUser.picture,
-        `${googleUser.email}-${new Date().toISOString()}.png`
-    );
-
-    if (!spacesImageUrl) {
-        throw new Error('Failed to upload image');
-    }
-
-    return spacesImageUrl.replace(
-        'nyc3.digitaloceanspaces',
-        'nyc3.cdn.digitaloceanspaces'
-    );
-}
-
-// TSDocs
-/**
- * Create a new user in the database
- * @param googleUser - The Google user object
- * @param imageUrl - The URL of the image
- * @returns The user object if created successfully
- */
-async function createNewUser(
-    googleUser: GoogleUser,
-    imageUrl: string
-): Promise<User> {
-    await db
-        .insertInto('users')
-        .values({
-            email: googleUser.email,
-            imageUrl: imageUrl,
-            interests: [],
-            location: '',
-            major: '',
-            name: googleUser.name,
-            year: '',
-        })
-        .execute();
-
-    const newUser = await findUserByEmail(googleUser.email);
-
-    if (!newUser) {
-        throw new Error('Failed to create new user');
-    }
-
-    return newUser;
-}
--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,6 +1,10 @@
 // OAuth 2.0
+import { Session, redirect } from '@remix-run/node';
+import { db } from 'db/src';
 import { google } from 'googleapis';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, APP_URL } from '~/utils/env';
+import { uploadImageFromCDN } from './s3-config.server';
+import { commitSession } from './session.server';
 
 export type GoogleUser = {
     id: string;
@@ -11,6 +15,19 @@ export type GoogleUser = {
     family_name: string;
     picture: string;
 };
+
+interface User {
+    name: string;
+    createdAt: Date | null;
+    id: string;
+    imageUrl: string | null; // Change this to allow null
+    location: string | null;
+    updatedAt: Date | null;
+    email: string;
+    interests: string[] | null;
+    major: string;
+    year: string;
+}
 
 /**
  * Google OAuth2 client
@@ -94,4 +111,114 @@ export async function exchangeCodeForToken(code: string) {
         refreshToken: tokens.refresh_token || '',
         accessToken: tokens.access_token || '',
     };
+}
+
+/**
+ * Find a user by their email
+ * @param email - The email of the user
+ * @returns The user object if found, otherwise undefined
+ */
+export async function findUserByEmail(
+    email: string
+): Promise<User | undefined> {
+    const userProfile = await db
+        .selectFrom('users')
+        .selectAll()
+        .where('email', '=', email)
+        .executeTakeFirst();
+
+    return userProfile;
+}
+
+/**
+ * Logs in an existing user and redirects them to the home page
+ * @param user - The user object
+ * @param session - The session object
+ * @returns A redirect response
+ */
+export async function loginExistingUser(user: User, session: Session) {
+    session.set('user_id', user.id);
+
+    const redirectUrl = new URL(`${APP_URL}/home`);
+    return redirect(redirectUrl.toString(), {
+        headers: {
+            'Set-Cookie': await commitSession(session),
+        },
+    });
+}
+
+/**
+ * Signs up a new user, adds them to the database, and redirects them to the home page
+ * @param googleUser - The Google user object
+ * @param session - The session object
+ * @returns A redirect response
+ */
+export async function signupNewUser(googleUser: GoogleUser, session: Session) {
+    const imageUrl = await uploadAndProcessImage(googleUser);
+    const newUser = await createNewUser(googleUser, imageUrl);
+
+    session.set('user_id', newUser.id);
+
+    const redirectUrl = new URL(`${APP_URL}/home`);
+    return redirect(redirectUrl.toString(), {
+        headers: {
+            'Set-Cookie': await commitSession(session),
+        },
+    });
+}
+
+/**
+ * Uploads the image from the Google user to DigitalOcean Spaces
+ * @param googleUser - The Google user object
+ * @returns The URL of the uploaded image
+ */
+export async function uploadAndProcessImage(
+    googleUser: GoogleUser
+): Promise<string> {
+    const spacesImageUrl = await uploadImageFromCDN(
+        googleUser.picture,
+        `${googleUser.email}-${new Date().toISOString()}.png`
+    );
+
+    if (!spacesImageUrl) {
+        throw new Error('Failed to upload image');
+    }
+
+    return spacesImageUrl.replace(
+        'nyc3.digitaloceanspaces',
+        'nyc3.cdn.digitaloceanspaces'
+    );
+}
+
+// TSDocs
+/**
+ * Create a new user in the database
+ * @param googleUser - The Google user object
+ * @param imageUrl - The URL of the image
+ * @returns The user object if created successfully
+ */
+async function createNewUser(
+    googleUser: GoogleUser,
+    imageUrl: string
+): Promise<User> {
+    await db
+        .insertInto('users')
+        .values({
+            email: googleUser.email,
+            imageUrl: imageUrl,
+            interests: [],
+            location: '',
+            major: '',
+            name: googleUser.name,
+            year: '',
+        })
+        .execute();
+
+    const newUser = await findUserByEmail(googleUser.email);
+
+    if (!newUser) {
+        throw new Error('Failed to create new user');
+    }
+
+    return newUser;
 }
