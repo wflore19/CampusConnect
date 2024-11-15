b4edd222b935b32111ccdbe93fd5595c5b53eabc
wflore19 <wflore19@montgomerycollege.edu>
2024-11-14 23:26:26 -0500

    fix: remove jwt

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,15 +1,7 @@
 // OAuth 2.0
-import { match } from 'ts-pattern';
 import { z } from 'zod';
 import { google } from 'googleapis';
-import jwt from 'jsonwebtoken';
-import {
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    APP_URL,
-    JWT_SECRET,
-} from '~/utils/env';
-import { db } from '../../db/src/shared/db';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, APP_URL } from '~/utils/env';
 
 const OAuthCodeState = z.object({
     clientRedirectUrl: z.string().url(),
@@ -31,10 +23,6 @@ const OAuthCodeState = z.object({
 // Types
 type OAuthCodeState = z.infer<typeof OAuthCodeState>;
 
-type HandleLoginInput = {
-    query: AuthorizationCodeQuery;
-    type: 'google';
-};
 type AuthorizationCodeQuery = z.infer<typeof AuthorizationCodeQuery>;
 export type OAuthServiceType = 'google';
 export type OAuthLoginInput = {
@@ -47,9 +35,6 @@ export type OuthLoginOutput = {
     authToken: string;
     email: string;
 };
-type SignTokenOptions = Partial<{
-    expiresIn: string;
-}>;
 export type OAuthTokens = {
     accessToken: string;
     refreshToken: string;
@@ -86,113 +71,6 @@ export type GoogleUser = {
     family_name: string;
     picture: string;
 };
-
-export async function handleLogin({ query, type }: HandleLoginInput) {
-    const { code, state } = query;
-
-    return match(state)
-        .with({ context: 'member_login' }, async () => {
-            // Typically workflow for login with google authentication using OAuth2 and Remix
-            // 1. Get the user's profile information from Google
-            // 2. Check if the user exists in the database
-            const { authToken, email } = await loginWithOAuth({
-                context: state.context,
-                code,
-                oauthRedirectUrl: state.oauthRedirectUrl,
-                type,
-            });
-
-            const url = new URL(state.clientRedirectUrl!);
-
-            if (authToken) {
-                url.searchParams.set('token', authToken);
-                url.searchParams.set('method', type);
-            } else {
-                url.searchParams.set(
-                    'error',
-                    `There was no user found with this email (${email}).`
-                );
-            }
-
-            return url.toString();
-        })
-        .with({ context: 'member_signup' }, async () => {
-            const googleUser = await getGoogleUser(code);
-
-            if (!googleUser) {
-                return { error: 'Failed to get Google user' };
-            }
-
-            const url = new URL(state.clientRedirectUrl!);
-
-            // Check if the user already exists
-            const existingUser = await db
-                .selectFrom('users')
-                .select('email')
-                .where('email', '=', googleUser.email)
-                .executeTakeFirst();
-
-            if (existingUser) {
-                url.searchParams.set(
-                    'error',
-                    'User with this email already exists'
-                );
-            } else {
-                // Create a new user
-                await db
-                    .insertInto('users')
-                    .values({
-                        email: googleUser.email,
-                        imageUrl: googleUser.picture,
-                        interests: [],
-                        location: '',
-                        major: '',
-                        name: googleUser.name,
-                        year: '',
-                    })
-                    .executeTakeFirst();
-            }
-
-            return '/home';
-        })
-        .exhaustive();
-}
-
-/**
- * Login with OAuth
- * @param input - The OAuth login input
- * @returns The OAuth login output
- */
-export async function loginWithOAuth(input: OAuthLoginInput) {
-    const profile = await getGoogleUser(input.code);
-
-    if (!profile) {
-        return {
-            authToken: '',
-            email: '',
-        };
-    }
-
-    const entity = await db
-        .selectFrom('users')
-        .select(['id'])
-        .where('email', '=', profile.email)
-        .executeTakeFirst();
-
-    if (!entity) {
-        return {
-            authToken: '',
-            email: profile.email,
-        };
-    }
-
-    const authToken = signToken({ id: entity.id }, { expiresIn: '1m' });
-
-    return {
-        authToken,
-        email: profile.email,
-    };
-}
 
 /**
  * Google OAuth2 client
@@ -276,20 +154,4 @@ export async function exchangeCodeForToken(code: string) {
         refreshToken: tokens.refresh_token || '',
         accessToken: tokens.access_token || '',
     };
-}
-
-/**
- * Sign a token with the provided data
- * @param data - The data to sign
- * @param options - The options for the token
- * @returns The signed token
- */
-export function signToken<T extends object>(
-    data: T,
-    options: SignTokenOptions = {}
-): string {
-    if (typeof JWT_SECRET !== 'string' || JWT_SECRET.length === 0) {
-        throw new Error('JWT_SECRET is not properly configured');
-    }
-    return jwt.sign(data, JWT_SECRET, options);
 }
