df5a135b313b64bbe5c91593f74c667235fafaca
wflore19 <wflore19@montgomerycollege.edu>
2024-11-18 14:30:52 -0500

    fix: create migrate script

--- db/src/scripts/create-migration.ts
+++ db/src/scripts/create-migration.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import prompt from 'prompt-sync';
 import { fileURLToPath } from 'url';
 
-import { run } from '@oyster/utils';
+import { run } from '../shared/utils';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,24 +12,24 @@ const __dirname = path.dirname(__filename);
 // Copied this beautiful piece of code from knex's codebase, see here:
 // https://github.com/knex/knex/blob/master/lib/migrations/util/timestamp.js
 const timestamp = run(() => {
-  const now = new Date();
+    const now = new Date();
 
-  return (
-    now.getUTCFullYear().toString() +
-    (now.getUTCMonth() + 1).toString().padStart(2, '0') +
-    now.getUTCDate().toString().padStart(2, '0') +
-    now.getUTCHours().toString().padStart(2, '0') +
-    now.getUTCMinutes().toString().padStart(2, '0') +
-    now.getUTCSeconds().toString().padStart(2, '0')
-  );
+    return (
+        now.getUTCFullYear().toString() +
+        (now.getUTCMonth() + 1).toString().padStart(2, '0') +
+        now.getUTCDate().toString().padStart(2, '0') +
+        now.getUTCHours().toString().padStart(2, '0') +
+        now.getUTCMinutes().toString().padStart(2, '0') +
+        now.getUTCSeconds().toString().padStart(2, '0')
+    );
 });
 
 const response = prompt()('Enter a migration name: ');
 const name = response.toLowerCase().trim();
 
 writeFileSync(
-  path.join(__dirname, `../migrations/${timestamp}_${name}.ts`),
-  dedent`
+    path.join(__dirname, `../migrations/${timestamp}_${name}.ts`),
+    dedent`
     import { type Kysely } from 'kysely';
 
     export async function up(db: Kysely<any>) {}
--- db/src/shared/utils.ts
+++ db/src/shared/utils.ts
@@ -1,10 +1,14 @@
 import { type RawBuilder, sql } from 'kysely';
 
 type Point = {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 };
 
 export function point({ x, y }: Point): RawBuilder<Point> {
-  return sql<Point>`point(${x}, ${y})`;
+    return sql<Point>`point(${x}, ${y})`;
+}
+
+export function run<T>(fn: () => T): T {
+    return fn();
 }
