f15e3418c238ccbe06aed7104095fcb20eff6b66
wflore19 <wflore19@montgomerycollege.edu>
2024-11-19 18:43:22 -0500

    fix: anaylytics

--- app/components/google-analytics.tsx
+++ app/components/google-analytics.tsx
@@ -1,28 +1,24 @@
-export function GoogleAnalytics({ GoogleAnalyticsId }: { GoogleAnalyticsId: string | undefined }) {
-   if (!GoogleAnalyticsId) {
-    return null;
-   }
-
-   return (
-      <>
-      {/* Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GoogleAnalyticsId}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+export function GoogleAnalytics() {
+    return (
+        <>
+            {/* Google Analytics */}
+            <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=G-C8XCRGVF02`}
+            />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
-              gtag('config', '${GoogleAnalyticsId}', {
+              gtag('config', 'G-C8XCRGVF02', {
                 page_path: window.location.pathname,
               });
             `,
-          }}
-        />
-      </>
-   )
+                }}
+            />
+        </>
+    );
 }
