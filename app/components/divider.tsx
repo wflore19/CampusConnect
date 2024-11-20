d09ad5a010c771dbfbe690ac2ceab613113ef45d
wflore19 <wflore19@montgomerycollege.edu>
2024-11-19 23:30:10 -0500

    chore: format

--- app/components/divider.tsx
+++ app/components/divider.tsx
@@ -3,21 +3,21 @@ import { match } from 'ts-pattern';
 import { cx } from '../utils/cx';
 
 type DividerProps = {
-  my?: '2' | '4';
+    my?: '2' | '4';
 };
 
 export function Divider({ my }: DividerProps) {
-  return (
-    <hr
-      className={cx(
-        'm-0 w-full border border-white border-opacity-35',
+    return (
+        <hr
+            className={cx(
+                'm-0 w-full border border-black border-opacity-10',
 
-        match(my)
-          .with('2', () => 'my-2')
-          .with('4', () => 'my-4')
-          .with(undefined, () => 'my-0')
-          .exhaustive()
-      )}
-    />
-  );
+                match(my)
+                    .with('2', () => 'my-2')
+                    .with('4', () => 'my-4')
+                    .with(undefined, () => 'my-0')
+                    .exhaustive()
+            )}
+        />
+    );
 }
