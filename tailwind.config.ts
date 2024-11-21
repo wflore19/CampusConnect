3348047cc372f9370420ac930c9e0d2cdf1378a2
wflore19 <wflore19@montgomerycollege.edu>
2024-11-21 01:55:20 -0500

    chore: format & add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,62 +1,64 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
 
-
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
-      },
-      colors: {
-        error: colors.red[600],
-        gold: '#fdb532',
-        'gold-100': '#fff7ea',
-        primary: '#dbeafe',
-        success: colors.green[600],
-        warning: colors.yellow[400],
-      },
-      keyframes: {
-        'loader-shimmer': {
-          '0%, 100%': { opacity: '0.1' },
-          '50%': { opacity: '0.8' },
+    content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    'Inter',
+                    'ui-sans-serif',
+                    'system-ui',
+                    'sans-serif',
+                    'Apple Color Emoji',
+                    'Segoe UI Emoji',
+                    'Segoe UI Symbol',
+                    'Noto Color Emoji',
+                ],
+            },
+            colors: {
+                error: colors.red[600],
+                gold: '#fdb532',
+                'gold-100': '#fff7ea',
+                primary: '#DC3E42',
+                'primary-light': 'rgba(220, 62, 66, 0.05)',
+                'primary-active': 'rgba(220, 62, 66, 0.15)',
+                'primary-current': 'rgba(220, 62, 66, 0.20)',
+                success: colors.green[600],
+                warning: colors.yellow[400],
+            },
+            keyframes: {
+                'loader-shimmer': {
+                    '0%, 100%': { opacity: '0.1' },
+                    '50%': { opacity: '0.8' },
+                },
+                'modal-animation': {
+                    '0%': { opacity: '0', transform: 'scale(0)' },
+                    '100%': { opacity: '1', transform: 'scale(1)' },
+                },
+                'modal-animation-mobile': {
+                    '0%': { bottom: '-100vh' },
+                    '100%': { bottom: '0vh' },
+                },
+                'modal-shader-animation': {
+                    '0%': { opacity: '0' },
+                    '100%': { opacity: '0.75' },
+                },
+                'toast-animation': {
+                    '0%': { transform: 'translateX(-150%)' },
+                    '100%': { transform: 'translateX(0%)' },
+                },
+                'toast-shader-animation': {
+                    '0%': { width: '100%' },
+                    '100%': { width: '0%' },
+                },
+                'slide-from-left': {
+                    '0%': { left: '-100%' },
+                    '100%': { left: '0%' },
+                },
+            },
         },
-        'modal-animation': {
-          '0%': { opacity: '0', transform: 'scale(0)' },
-          '100%': { opacity: '1', transform: 'scale(1)' },
-        },
-        'modal-animation-mobile': {
-          '0%': { bottom: '-100vh' },
-          '100%': { bottom: '0vh' },
-        },
-        'modal-shader-animation': {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '0.75' },
-        },
-        'toast-animation': {
-          '0%': { transform: 'translateX(-150%)' },
-          '100%': { transform: 'translateX(0%)' },
-        },
-        'toast-shader-animation': {
-          '0%': { width: '100%' },
-          '100%': { width: '0%' },
-        },
-        'slide-from-left': {
-          '0%': { left: '-100%' },
-          '100%': { left: '0%' },
-        },
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
