2d718442852433bbd7c728fb51c87187de55e9fa
wflore19 <wflore19@montgomerycollege.edu>
2024-11-21 01:55:41 -0500

    add: translucent prop to Theme

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <Links />
             </head>
             <body>
-                <Theme accentColor="red">
+                <Theme accentColor="red" panelBackground="translucent">
                     {children}
                     {/* <ThemePanel /> */}
                 </Theme>
