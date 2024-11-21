79f5c3c4106076fbadaf44370a5ce19b03e074e0
wflore19 <wflore19@montgomerycollege.edu>
2024-11-21 03:20:26 -0500

    fix: profile layout

--- app/routes/_dashboard.profile._index.tsx
+++ app/routes/_dashboard.profile._index.tsx
@@ -2,7 +2,15 @@ import { LoaderFunctionArgs } from '@remix-run/node';
 import { useLoaderData, useRouteLoaderData } from '@remix-run/react';
 import { getFriendsList } from '~/modules/friends/friends.core';
 import { getSession, user } from '~/utils/session.server';
-import { Avatar, Flex, Heading, Text } from '@radix-ui/themes';
+import {
+    Avatar,
+    Box,
+    Card,
+    Flex,
+    Heading,
+    Text,
+    Separator,
+} from '@radix-ui/themes';
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const session = await getSession(request);
@@ -10,12 +18,12 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
     const friendsList = await getFriendsList(id);
 
-    return friendsList.length > 0 ? friendsList : [];
+    return { friendsList: friendsList.length > 0 ? friendsList : [] };
 }
 
 export default function MyProfile() {
-    const friendsList = useLoaderData<typeof loader>();
-    const { firstName, lastName, profilePicture } = useRouteLoaderData(
+    const { friendsList } = useLoaderData<typeof loader>();
+    const { firstName, lastName, email, profilePicture } = useRouteLoaderData(
         'routes/_dashboard'
     ) as {
         firstName: string;
@@ -25,26 +33,47 @@ export default function MyProfile() {
     };
 
     return (
-        <Flex direction={'column'}>
-            <Heading as="h1" size={'9'}>
-                {firstName} {lastName}
+        <>
+            <Heading size="7" mb="4">
+                My Profile
             </Heading>
 
-            <Flex direction={'column'} gap={'3'}>
-                <Flex gap="2">
-                    <Avatar
-                        src={profilePicture}
-                        size={'8'}
-                        radius="full"
-                        fallback="A"
-                    />
-                </Flex>
+            <Separator size="4" mb="4" />
 
-                <Flex gap={'1'}>
-                    <Text className="font-semibold">Friends</Text>
-                    <Text>{friendsList.length}</Text>
+            <Card>
+                <Flex direction="column" gap="4">
+                    <Avatar
+                        size="8"
+                        src={profilePicture}
+                        fallback={`${firstName[0]}${lastName[0]}`}
+                        mb="2"
+                    />
+                    <Box>
+                        <Text as="div" size="2" weight="bold" mb="1">
+                            Name
+                        </Text>
+                        <Text as="div" size="3">
+                            {firstName} {lastName}
+                        </Text>
+                    </Box>
+                    <Box>
+                        <Text as="div" size="2" weight="bold" mb="1">
+                            Email
+                        </Text>
+                        <Text as="div" size="3">
+                            {email}
+                        </Text>
+                    </Box>
+                    <Box>
+                        <Text as="div" size="2" weight="bold" mb="1">
+                            Friends
+                        </Text>
+                        <Text as="div" size="3">
+                            {friendsList.length}
+                        </Text>
+                    </Box>
                 </Flex>
-            </Flex>
-        </Flex>
+            </Card>
+        </>
     );
 }
