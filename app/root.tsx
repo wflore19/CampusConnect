2875238c168890fa59d6ceb94414a69260f51e3e
wflore19 <wflore19@montgomerycollege.edu>
2024-11-21 10:42:59 -0500

    chore: light mode

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className="dark-theme">
+        <html lang="en">
             <head>
                 <meta charSet="utf-8" />
                 <meta
