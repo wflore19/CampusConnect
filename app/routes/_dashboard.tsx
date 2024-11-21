54fe88dd050b78e336f6bea808e196473815f45b
wflore19 <wflore19@montgomerycollege.edu>
2024-11-21 02:55:52 -0500

    add: mobile drawer, sidebar for tablet and desktop

--- app/routes/_dashboard.tsx
+++ app/routes/_dashboard.tsx
@@ -1,11 +1,10 @@
+// app/routes/_dashboard.tsx
 import { LoaderFunction } from '@remix-run/node';
-import { Dashboard } from '../components/dashboard';
-import { Divider } from '../components/divider';
 import { Outlet } from '@remix-run/react';
-import { Calendar, Layers, User, Users } from 'react-feather';
+import { Dashboard } from '~/components/dashboard';
 import { ensureUserAuthenticated, user } from '~/utils/session.server';
 import { db } from 'db/src';
-import { Box } from '@radix-ui/themes';
+import { Box, Container } from '@radix-ui/themes';
 
 export const loader: LoaderFunction = async ({ request }) => {
     const session = await ensureUserAuthenticated(request);
@@ -27,52 +26,14 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function DashboardLayout() {
     return (
-        <Dashboard>
-            <Dashboard.Header />
-            <Dashboard.Sidebar>
-                <Box className="mb-8 flex w-full items-center gap-1 p-6">
-                    <Dashboard.CompanyLogo />
-                </Box>
-
-                <Dashboard.Navigation>
-                    <Dashboard.NavigationList>
-                        <Dashboard.NavigationLink
-                            icon={<Layers />}
-                            label={`Home`}
-                            pathname={'/home'}
-                        />
-                        <Dashboard.NavigationLink
-                            icon={<Calendar />}
-                            label="Events"
-                            pathname={'/events'}
-                        />
-                        <Dashboard.NavigationLink
-                            icon={<User />}
-                            label="Users"
-                            pathname={'/users'}
-                        />
-                        <Dashboard.NavigationLink
-                            icon={<Users />}
-                            label="Friends"
-                            pathname={'/friends'}
-                        />
-                        <Dashboard.NavigationLink
-                            icon={<User />}
-                            label="Profile"
-                            pathname={'/profile'}
-                        />
-
-                        <div className="mb-2 mt-10">
-                            <Divider />
-                        </div>
-                        <Dashboard.LogoutForm />
-                    </Dashboard.NavigationList>
-                </Dashboard.Navigation>
-            </Dashboard.Sidebar>
-
-            <Dashboard.Page className="overflow-auto">
-                <Outlet />
-            </Dashboard.Page>
-        </Dashboard>
+        <Box style={{ maxWidth: '1300px', margin: '0 auto', width: '100%' }}>
+            <Dashboard>
+                <Container size="3">
+                    <Box py="6">
+                        <Outlet />
+                    </Box>
+                </Container>
+            </Dashboard>
+        </Box>
     );
 }
