d61fcd52f5f60718621fb8a06502dbf8b7d758c6
wflore19 <wflore19@montgomerycollege.edu>
2024-11-26 19:11:42 -0500

    chore: format

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -9,98 +9,98 @@ import { useHydrated } from '~/hooks/useHydrated';
 import { cx } from '../utils/cx';
 
 const ModalContext = React.createContext({
-  _initialized: false,
-  onCloseTo: '' as LinkProps['to'],
+    _initialized: false,
+    onCloseTo: '' as LinkProps['to'],
 });
 
 export function useIsModalParent() {
-  const { _initialized } = useContext(ModalContext);
+    const { _initialized } = useContext(ModalContext);
 
-  return !!_initialized;
+    return !!_initialized;
 }
 
 type ModalProps = PropsWithChildren<{
-  onCloseTo: LinkProps['to'];
-  size?: '400' | '600';
+    onCloseTo: LinkProps['to'];
+    size?: '400' | '600';
 }>;
 
 export const Modal = ({
-  children,
-  onCloseTo,
-  size = '600',
+    children,
+    onCloseTo,
+    size = '600',
 }: ModalProps): JSX.Element | null => {
-  const hydrated = useHydrated();
+    const hydrated = useHydrated();
 
-  if (!hydrated) {
-    return null;
-  }
+    if (!hydrated) {
+        return null;
+    }
 
-  return createPortal(
-    <ModalContext.Provider value={{ _initialized: true, onCloseTo }}>
-      <div
-        className={cx(
-          'fixed flex h-screen w-screen justify-center',
-          'bottom-0 items-center', // Mobile
-          'sm:top-0' // > Mobile
-        )}
-      >
-        <aside
-          className={cx(
-            'lock-scroll relative z-10 flex max-h-[calc(100vh-5rem)] max-w-[400px] flex-col gap-4 overflow-auto bg-white p-4',
-            'animate-[modal-animation-mobile_250ms] rounded-t-lg',
-            'sm:animate-[modal-animation_250ms] sm:rounded-lg',
-            size === '400' && 'sm:max-w-[400px]',
-            size === '600' && 'sm:max-w-[600px]'
-          )}
-          id="modal"
-          role="dialog"
-        >
-          {children}
-        </aside>
+    return createPortal(
+        <ModalContext.Provider value={{ _initialized: true, onCloseTo }}>
+            <div
+                className={cx(
+                    'fixed flex h-screen w-screen justify-center',
+                    'bottom-0 items-center', // Mobile
+                    'sm:top-0' // > Mobile
+                )}
+            >
+                <aside
+                    className={cx(
+                        'lock-scroll relative z-10 flex max-h-[calc(100vh-5rem)] w-[400px] flex-col gap-4 overflow-auto bg-white p-4',
+                        'animate-[modal-animation-mobile_250ms] rounded-t-lg',
+                        'sm:animate-[modal-animation_250ms] sm:rounded-lg',
+                        size === '400' && 'max-w-[400px]',
+                        size === '600' && 'max-w-[600px]'
+                    )}
+                    id="modal"
+                    role="dialog"
+                >
+                    {children}
+                </aside>
 
-        <Link
-          className={cx(
-            'absolute inset-0 cursor-default bg-black',
-            'animate-[modal-shader-animation_250ms_forwards]'
-          )}
-          preventScrollReset
-          to={onCloseTo}
-        />
-      </div>
-    </ModalContext.Provider>,
-    document.body
-  );
+                <Link
+                    className={cx(
+                        'absolute inset-0 cursor-default bg-black',
+                        'animate-[modal-shader-animation_250ms_forwards]'
+                    )}
+                    preventScrollReset
+                    to={onCloseTo}
+                />
+            </div>
+        </ModalContext.Provider>,
+        document.body
+    );
 };
 
 Modal.CloseButton = function ModalCloseButton() {
-  const { onCloseTo } = useContext(ModalContext);
+    const { onCloseTo } = useContext(ModalContext);
 
-  return (
-    <Link
-      className={getIconButtonCn({
-        backgroundColor: 'gray-100',
-        backgroundColorOnHover: 'gray-200',
-      })}
-      preventScrollReset
-      to={onCloseTo}
-    >
-      <X />
-    </Link>
-  );
+    return (
+        <Link
+            className={getIconButtonCn({
+                backgroundColor: 'gray-100',
+                backgroundColorOnHover: 'gray-200',
+            })}
+            preventScrollReset
+            to={onCloseTo}
+        >
+            <X />
+        </Link>
+    );
 };
 
 Modal.Description = function ModalDescription({ children }: PropsWithChildren) {
-  return <Text color="gray-500">{children}</Text>;
+    return <Text color="gray-500">{children}</Text>;
 };
 
 Modal.Header = function ModalHeader({ children }: PropsWithChildren) {
-  return (
-    <header className="flex items-start justify-between gap-2">
-      {children}
-    </header>
-  );
+    return (
+        <header className="flex items-start justify-between gap-2">
+            {children}
+        </header>
+    );
 };
 
 Modal.Title = function ModalTitle({ children }: PropsWithChildren) {
-  return <Text variant="xl">{children}</Text>;
+    return <Text variant="xl">{children}</Text>;
 };
