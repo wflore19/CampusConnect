ff35424d8bbde3e8f8f038961065b1c778bcd606
wflore19 <wflore19@montgomerycollege.edu>
2024-12-02 20:28:52 -0500

    chore: add TSDocs

--- apps/campusconnect/app/hooks/useHydrated.ts
+++ apps/campusconnect/app/hooks/useHydrated.ts
@@ -1,9 +1,18 @@
-import React from "react";
+import React from 'react';
 
+/**
+ * This hook helps determine if the client-side JavaScript has been fully
+ * loaded and hydrated with the server-rendered content. It's particularly
+ * useful for handling functionality that should only run on the client side
+ * or for preventing hydration mismatches.
+ *
+ * @returns {boolean} Returns true when the component is hydrated on the client side,
+ *                    false during server-side rendering or before hydration is complete.
+ */
 export const useHydrated = () => {
     const [hydrated, setHydrated] = React.useState(false);
     React.useEffect(() => {
-        if (typeof window === "undefined") {
+        if (typeof window === 'undefined') {
             return;
         }
         setHydrated(false);
--- apps/campusconnect/app/utils/bullmq.ts
+++ apps/campusconnect/app/utils/bullmq.ts
@@ -1,10 +1,23 @@
 import { Queue, Worker, Job } from 'bullmq';
 import { redis } from './redis';
 
+/**
+ * Create a new queue with the given name.
+ * A queue is a list of jobs that are waiting to be processed.
+ * @param name
+ * @returns
+ */
 export function createQueue(name: string) {
     return new Queue(name, { connection: redis });
 }
 
+/**
+ * Create a new worker with the given name and processor.
+ * A worker is a process that processes jobs from a queue.
+ * @param name - The name of the worker.
+ * @param processor - The function that processes the job.
+ * @returns
+ */
 export function createWorker<T>(
     name: string,
     processor: (job: Job) => Promise<T>
