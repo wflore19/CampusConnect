3b393a69005de5e807cc73d79f580268b46f50dd
wflore19 <wflore19@montgomerycollege.edu>
2024-12-10 07:32:11 -0500

    add: import/order rule

--- apps/campusconnect/.eslintrc.js
+++ apps/campusconnect/.eslintrc.js
@@ -1,10 +1,26 @@
 /** @type {import("eslint").Linter.Config} */
-export default [{
-    extends: ['@oyster/eslint-config/base'],
-    parserOptions: {
-      tsconfigRootDir: __dirname,
+export default [
+    {
+        extends: ['@oyster/eslint-config/base'],
+        parserOptions: {
+            tsconfigRootDir: __dirname,
+        },
+        rules: {
+            'no-restricted-imports': ['error', { patterns: ['./*', '../*'] }],
+            'import/order': [
+                WARN,
+                {
+                    alphabetize: { caseInsensitive: true, order: 'asc' },
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        'parent',
+                        'sibling',
+                    ],
+                    'newlines-between': 'always',
+                },
+            ],
+        },
     },
-    rules: {
-      'no-restricted-imports': ['error', { patterns: ['./*', '../*'] }],
-    },
-}];
+];
